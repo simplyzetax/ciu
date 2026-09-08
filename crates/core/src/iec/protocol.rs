4d34645ec30811d11adfc7822d40ae3c950aab55
Finn <ernzerhofffinn@icloud.com>
2026-09-08 19:44:57 +0200

    feat: Base message protocol

---
+++ crates/core/src/iec/protocol.rs
@@ -0,0 +1,61 @@
+macro_rules! messages {
+    (
+        $(
+            $name:ident = $id:literal => $payload:ty
+        ),* $(,)?
+    ) => {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        #[repr(u8)]
+        pub enum MessageType {
+            $(
+                $name = $id,
+            )*
+        }
+
+        #[derive(Debug, Clone, Copy, PartialEq)]
+        pub enum Message {
+            $(
+                $name($payload),
+            )*
+        }
+
+        impl Message {
+            pub fn message_type(&self) -> MessageType {
+                match self {
+                    $(
+                        Self::$name(_) => MessageType::$name,
+                    )*
+                }
+            }
+        }
+    };
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum ABSMode {
+    Street = 1,
+    Supermoto = 2,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Throttle {
+    pub amount: u8,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Rpm {
+    pub amount: u16,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Clutch {
+    pub engaged: bool,
+}
+
+messages! {
+    Throttle = 1 => Throttle,
+    ABS      = 2 => ABSMode,
+    Rpm      = 3 => Rpm,
+    Clutch   = 4 => Clutch,
+}
