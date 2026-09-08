3bd67fe0b83e6d424915ac9020cc7b39ab5c23e1
Finn <ernzerhofffinn@icloud.com>
2026-09-08 19:31:29 +0200

    feat: Proper project setup

--- crates/goggles/src/main.rs
+++ crates/goggles/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use esp_idf_hal::{delay::FreeRtos, gpio::PinDriver, peripherals::Peripherals};
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_svc::sys::link_patches();
+
+    let peripherals = Peripherals::take()?;
+
+    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
+
+    loop {
+        led.set_high()?;
+        FreeRtos::delay_ms(500);
+
+        led.set_low()?;
+        FreeRtos::delay_ms(500);
+    }
 }
